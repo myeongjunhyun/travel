6bdf4aeae907cb77153d3c1b86fc6bd1ab2e6c5d
mjhyun <119093998+myeongjunhyun@users.noreply.github.com>
2026-02-13 00:57:12 +0900

    travel

--- lib/fileSystem.ts
+++ lib/fileSystem.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 
 const TRIP_ASSETS_DIR = FileSystem.documentDirectory + 'trip_assets/';
 
